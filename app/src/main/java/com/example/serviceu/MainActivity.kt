e3db920f9568e84a94b9e615a304364f4e80f1d2
Glaiza Maylan <darlenemaylan04@gmail.com>
2023-10-03 14:11:54 +0800

    login page

--- app/src/main/java/com/example/serviceu/MainActivity.kt
+++ app/src/main/java/com/example/serviceu/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.serviceu
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button=findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
-}
+} 
