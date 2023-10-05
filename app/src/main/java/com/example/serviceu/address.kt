c73249e65ed195d22904c85ebf627c620ae9d188
Vinna Lauraine Gutierrez <vinnalauraine@gmail.com>
2023-10-05 21:57:04 +0800

    added address interface

---
+++ app/src/main/java/com/example/serviceu/address.kt
@@ -0,0 +1,71 @@
+package com.example.serviceu
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.TimePicker
+import java.time.Year
+import java.util.Calendar
+
+class address : AppCompatActivity(),DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
+
+    private lateinit var btn_datetimePicker: Button
+
+    private lateinit var tv_TextTime: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address)
+
+        btn_datetimePicker = findViewById(R.id.btn_datetimePicker)
+        tv_TextTime = findViewById(R.id.tv_TextTime)
+        pickDate()
+    }
+    private fun getDateTimeCalendar() {
+        val calendar = Calendar.getInstance()
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        month = calendar.get(Calendar.MONTH)
+        year = calendar.get(Calendar.YEAR)
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+    }
+
+    private fun pickDate() {
+        btn_datetimePicker.setOnClickListener {
+
+            getDateTimeCalendar()
+            DatePickerDialog(this, this,year,month,day).show()
+        }
+    }
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear = year
+
+        getDateTimeCalendar()
+
+        TimePickerDialog(this, this, hour, minute, false).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+        
+        tv_TextTime.text = "$savedDay-$savedMonth-$savedYear\n Hour: $savedHour Minute: $savedMinute"
+    }
+}
