d90918b6ae3ae15a673b87a36fa918ff23189685
Monica Villanueva <vllnv.monica@gmail.com>
2023-10-05 23:33:04 +0800

    bottomnav funtionality

--- app/src/main/java/com/example/serviceu/Services.kt
+++ app/src/main/java/com/example/serviceu/Services.kt
@@ -1,18 +1,42 @@
 package com.example.serviceu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Services : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var servicesList: ArrayList<ServicesClass>
     private lateinit var imageList: Array <Int>
     private lateinit var titleList: Array <String>
+    private lateinit var BottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_services)
+
+        BottomNavigationView = findViewById(R.id.navmenu)
+        BottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    val intent = Intent(this, Services::class.java)
+                    startActivity(intent)
+                    finish()}
+//                R.id.bookings -> {
+//                    val intent = Intent(this, Bookings::class.java)
+//                    startActivity(intent)
+//                    finish()}
+                R.id.profile->{
+                    val intent = Intent(this, Profile::class.java)
+                    startActivity(intent)
+                    finish()}
+                }
+                true
+            }
+
+
         imageList = arrayOf(
             R.drawable.laundry,
             R.drawable.electrician,
@@ -40,7 +64,7 @@ class Services : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
-        servicesList = arrayListOf<ServicesClass>()
+        servicesList = arrayListOf()
         getData()
     }
 
