6e1e19f5eb6310a37afb0ef7d3ab0853b94e056b
rheven <rhus.mendoza.up@phinmaed.com>
2023-10-12 20:50:57 +0800

    added service provider recycler view

---
+++ app/src/main/java/com/example/serviceu/spprof_rvAdapted.kt
@@ -0,0 +1,39 @@
+package com.example.serviceu
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class sp_profileAdapter(
+    private val getActivity: Sp_profile,
+    private val profilelists: MutableList<Sp_profile_holder>
+) :
+    RecyclerView.Adapter<sp_profileAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val  view = LayoutInflater.from(parent.context).inflate(R.layout.spprof_recyclerview_layout,parent, false)
+        return MyViewHolder(view)
+    }
+    override fun getItemCount(): Int {
+        return profilelists.size
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.name.text = profilelists[position].title
+        holder.profilepic.setImageResource( profilelists[position].img1)
+        holder.contact.text = profilelists[position].rad
+        holder.cardview.setOnClickListener{
+            Toast.makeText(getActivity, profilelists[position].title, Toast.LENGTH_LONG).show()
+        }
+    }
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val profilepic : ImageView = itemView.findViewById(R.id.img_profilepic)
+        val name : TextView = itemView.findViewById(R.id.tv_spname)
+        val contact : TextView = itemView.findViewById(R.id.tv_sp_number)
+        val cardview : CardView = itemView.findViewById(R.id.cardview)
+    }
+}
