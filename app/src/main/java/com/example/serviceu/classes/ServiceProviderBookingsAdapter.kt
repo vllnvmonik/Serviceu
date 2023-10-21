f184ab12bb9d91d99aee4de9c0c3b3f07115d470
rheven <rhus.mendoza.up@phinmaed.com>
2023-10-21 17:51:34 +0800

    added service provider recycler view

---
+++ app/src/main/java/com/example/serviceu/classes/ServiceProviderBookingsAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.serviceu.classes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.serviceu.R
+
+class ServiceProviderBookingsAdapter (
+    private val bookingsList: ArrayList<ServiceProviderBookingClass>
+): RecyclerView.Adapter<ServiceProviderBookingsAdapter.ViewHolderClass>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.service_provider_bookings_layout, parent, false)
+        return ViewHolderClass(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return bookingsList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+        val currentItem = bookingsList[position]
+        holder.rvImageHolder.setImageResource(currentItem.bookingsImg)
+        holder.rvTitleHolder.text = currentItem.serviceTitle
+        holder.rvDateHolder.text = currentItem.date
+        holder.rvTimeHolder.text = currentItem.time
+        // Uncomment and adapt if you have a status property:
+        // holder.rvStatusHolder.text = currentItem.status
+    }
+
+    class ViewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val rvImageHolder: ImageView = itemView.findViewById(R.id.img_bookings)
+        val rvTitleHolder: TextView = itemView.findViewById(R.id.tv_service_booked)
+        val rvDateHolder: TextView = itemView.findViewById(R.id.tv_date)
+        val rvTimeHolder: TextView = itemView.findViewById(R.id.tv_time)
+        // Uncomment and adapt if you have a status property:
+        // val rvStatusHolder: TextView = itemView.findViewById(R.id.booking_status)
+    }
+
+    fun setData(newData: List<ServiceProviderBookingClass>) {
+        bookingsList.clear()
+        bookingsList.addAll(newData)
+        notifyDataSetChanged()
+    }
+}
